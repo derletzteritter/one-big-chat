6d3adedf0770e6d8ab786d585d0cbf139b45b043
Christopher <onlygamernerd@gmail.com>
2021-03-18 18:36:07 +0100

    login page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
+  const [name, setName] = useState('');
+
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen bg-pink-500">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>One Big Chat</h1>
+      <main className="shadow-md rounded-md p-40 bg-pink-400">
+        <h1 className="text-white text-center font-bold text-3xl mb-5">
+          One Big Chat
+        </h1>
+        <div>
+          <input
+            placeholder="nickname"
+            className="rounded bg-gray-100 p-2 px-1 outline-none text-gray-400"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </div>
+        <div className="text-center">
+          <button className="text-white font-medium bg-pink-300 mt-2 p-2 rounded w-52 shadow">
+            Chat
+          </button>
+        </div>
       </main>
     </div>
   );
