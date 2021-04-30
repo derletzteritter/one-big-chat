8c2acfa3d70cd08c9168c38a292b6513bbc1420f
Christopher <onlygamernerd@gmail.com>
2021-04-30 15:34:27 +0200

    remove get from signup

--- server/auth/authRoutes.ts
+++ server/auth/authRoutes.ts
@@ -4,4 +4,3 @@ import { handleSignup } from './controller/authController';
 export const router = Router();
 
 router.post('/signup', handleSignup);
-router.get('/signup', (req, res) => res.send('hello'));
