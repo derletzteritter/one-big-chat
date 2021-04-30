3c465f198b1e2a2661cb11373c431593ac0d1081
Christopher <onlygamernerd@gmail.com>
2021-04-30 15:33:05 +0200

    cors options'

--- server/server.ts
+++ server/server.ts
@@ -13,13 +13,13 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    credentials: true,
   }),
 );
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
