6718c473dfd09be7249bfb1304bba485e6a2c262
Christopher <onlygamernerd@gmail.com>
2021-05-04 13:59:19 +0200

    xd

---
+++ client/src/video/VideoChat.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function VideoChat() {
+  return (
+    <div>
+      <video id="facecam" autoPlay playsInline muted></video>
+      <button></button>
+    </div>
+  );
+}
