4dc7363da9c8c7f40ffc49ee6bd82310e28c11ae
Christopher <onlygamernerd@gmail.com>
2021-06-04 08:50:21 +0200

    update auth provider

--- client/src/provider/AuthProvider.tsx
+++ client/src/provider/AuthProvider.tsx
@@ -4,9 +4,12 @@ export const AuthContext = createContext(null);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [userDetails, setUserDetails] = useState(null);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider
+      value={{ user, setUser, userDetails, setUserDetails }}
+    >
       {children}
     </AuthContext.Provider>
   );
