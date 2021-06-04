e8e8a28f6a64c22e9f5eac20fbf53fab1a552caa
Christopher <onlygamernerd@gmail.com>
2021-06-04 08:48:37 +0200

    update videochat

--- client/src/video/VideoChat.tsx
+++ client/src/video/VideoChat.tsx
@@ -1,11 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
 export default function VideoChat() {
-  const videoRef = useRef(null);
-  const [grayscale, setGrayscale] = useState<any>(0);
-  const [invert, setInvert] = useState<any>(0);
-  const video = document.querySelector('video');
-
   const handleWebcam = () => {
     navigator.mediaDevices
       .getUserMedia({ video: true })
@@ -13,7 +8,7 @@ export default function VideoChat() {
   };
 
   const handleStartStream = async (stream: MediaStream) => {
-    /*const video = document.querySelector('video');*/
+    const video = document.querySelector('video');
     video.srcObject = stream;
     video.controls = true;
   };
@@ -22,16 +17,6 @@ export default function VideoChat() {
     <div>
       <video autoPlay playsInline muted></video>
       <button onClick={handleWebcam}>Start Stream</button>
-      <h3>Grayscale</h3>
-      <h3>Invert</h3>
-      <input
-        type="range"
-        value={invert}
-        onChange={(e) => {
-          setInvert(e.currentTarget.value);
-          video.style.filter = `invert(${invert}%)`;
-        }}
-      />
     </div>
   );
 }
